6aab9e183091447d13bc80499e34ec6ad164ad6c
brianschnee <brian.a.schnee@gmail.com>
2022-07-22 14:11:46 -0400

    added comments to dropdown.js and scroll padding adjustment for mobile

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,17 +1,23 @@
 // Dropdown functionality
-document.addEventListener('click', (e) => {
-	const isDropdownButton = e.target.matches('[data-dropdown-button]');
-	if (!isDropdownButton && e.target.closest('[data-dropdown]') != null)
-		return;
 
-	let currentDropdown;
+// listen for a click anywhere in the document
+document.addEventListener('click', (e) => {
+	// check to see if dropdown button was clicked
+	const isDropdownButton = e.target.matches('[data-dropdown-button]')
+
+	// if the dropdown was clicked, toggle the active class and display the dropdown (css will apply the styling to make the dropdown appear)
 	if (isDropdownButton) {
-		currentDropdown = e.target.closest('[data-dropdown]');
-		currentDropdown.classList.toggle('active');
+		let currentDropdown = e.target.closest('[data-dropdown]')
+		currentDropdown.classList.toggle('active')
+	} else {
+		// if the click existed within the dropdown, ignore the click
+		if (e.target.closest('[data-dropdown]') != null) return
+
+		// if the active class isnt applied to the dropdown yet, ignore, otherwise remove the active class
+		const dropdown = document.querySelector('[data-dropdown].active')
+
+		if (dropdown === null) return
+		dropdown.classList.remove('active')
 	}
 
-	document.querySelectorAll('[data-dropdown].active').forEach((dropdown) => {
-		if (dropdown === currentDropdown) return;
-		dropdown.classList.remove('active');
-	});
-});
+})
