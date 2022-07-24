4303590d64dcf8f328430879041a48769c1a657b
brianschnee <brian.a.schnee@gmail.com>
2022-07-24 14:19:38 -0400

    changed styling of dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,20 +3,20 @@
 // listen for a click anywhere in the document
 document.addEventListener('click', (e) => {
 	// check to see if dropdown button was clicked
-	const isDropdownButton = e.target.matches('[data-dropdown-button]')
+	const isDropdownButton = e.target.matches('[data-dropdown-button]');
 
 	// if the dropdown was clicked, toggle the active class and display the dropdown (css will apply the styling to make the dropdown appear)
 	if (isDropdownButton) {
-		let currentDropdown = e.target.closest('[data-dropdown]')
-		currentDropdown.classList.toggle('active')
+		let currentDropdown = e.target.closest('[data-dropdown]');
+		currentDropdown.classList.toggle('active');
 	} else {
 		// if the click existed within the dropdown, ignore the click
-		if (e.target.closest('[data-dropdown]') != null) return
+		if (e.target.closest('[data-dropdown]') != null) return;
 
 		// if the active class isnt applied to the dropdown yet, ignore, otherwise remove the active class
-		const dropdown = document.querySelector('[data-dropdown].active')
+		const dropdown = document.querySelector('[data-dropdown].active');
 
-		if (dropdown === null) return
-		dropdown.classList.remove('active')
+		if (dropdown === null) return;
+		dropdown.classList.remove('active');
 	}
-})
+});
