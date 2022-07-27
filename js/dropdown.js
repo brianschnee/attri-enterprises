396ea2ca9b82da2bf604154b559071c9c99056b0
brianschnee <brian.a.schnee@gmail.com>
2022-07-27 12:56:07 -0400

    update project titles

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,3 +1,4 @@
+// Dropdown functionality
 document.addEventListener('click', (e) => {
 	const isDropdownButton = e.target.matches('[data-dropdown-button]');
 	if (!isDropdownButton && e.target.closest('[data-dropdown]') != null)
